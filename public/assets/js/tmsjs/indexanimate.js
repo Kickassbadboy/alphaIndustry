e6f8b3a6f1714d4c857785e1ab97450d29d386f5
Yash <yash.talegaonkar37@gmail.com>
2019-12-02 16:27:00 +0530

    images array for index
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

---
+++ public/assets/js/tmsjs/indexanimate.js
@@ -0,0 +1,15 @@
+/*
+Create an Array 
+var currImage ='';
+var var1=relativelink
+var var2=relativelink
+var var3=relativelink
+
+
+var images = [var1, var2, var3];
+
+for(i=0;i<images.length; i++)
+{
+    this.currImage = images[i];
+}
+*/
