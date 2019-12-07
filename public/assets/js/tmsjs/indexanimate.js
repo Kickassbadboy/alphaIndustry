a8b5b3f870a2328b9224f97a9680c2aef4a2b3d6
Sejal <sejalyerunkar30@gmail.com>
2019-12-07 22:51:42 +0530

    index page changes

--- public/assets/js/tmsjs/indexanimate.js
+++ public/assets/js/tmsjs/indexanimate.js
@@ -14,7 +14,7 @@ for(i=0;i<images.length; i++)
 }
 */
 
-window.onload = function() {
+window.onload = function () {
     imgchange();
 }
 
@@ -23,16 +23,34 @@ function imgchange() {
 
     var currimage;
 
-    var img1 = "assets/images/main-slider/core_box1.png";
-    var img2 = "assets/images/main-slider/slide2-hp1.jpg";
-    // var img3="assets/images/main-slider/core_box.png";
-    // var img4="assets/images/main-slider/core_box.png";
+    var img1 = "assets/images/Final folder/cover01.png";
+    var img2 = "assets/images/Final folder/cover2.png";
+    var img3 = "assets/images/Final folder/cover3.png";
+    var img4 = "assets/images/Final folder/cover4.png";
 
-    var images = [img1, img2];
+    var para1 = "<h5>Your partner in manufacturing of  foundry tooling, dies and fixtures.</h5><br>Alpha has consistently embraced innovation to provide reliable services to a wide range of industries.";
+    var para2 = "<h5>We offer a wide range of foundry solutions.With over 2 decades experience in this field,</h5> <br>Alpha has earned a high reputation in foundry tooling solutions.";
+    var para3 = "<h5>A proud team of experienced engineers with an innovative approach.</h5><br>Alpha has a qualified team of engineers and professionals ready to take on new industry challenges."
+    var para4 = "<h5>Allied expertise in design, simulation and reverse engineering.</h5><br>Alpha has established capabilities in designing of technically critical projects alongside customers by understanding their needs. <br>We have achieved competence in simulation of castings and design optimisation which guarantee reliable results."
+
+    var paragraph = [para1, para2, para3];
+    var paraindex = 0;
+    var images = [img1, img2, img3, img4];
     var index = 0;
 
+    setInterval(function () {
+        var element = document.getElementById("para01"); 
+        element.innerHTML = para1;
+        var element = document.getElementById("para02");
+        element.innerHTML = para2;
+        var element = document.getElementById("para03");
+        element.innerHTML = para3;
+        var element = document.getElementById("para04");
+        element.innerHTML = para4;
 
-    setInterval(function() {
+        console.log("Set Interval");
+        paraindex = (paraindex + 1) % paragraph.length;
+
         document.getElementById('img').src = images[index];
         console.log("Set Intervel");
         index = (index + 1) % images.length;
@@ -41,8 +59,6 @@ function imgchange() {
 
         // }
 
-    }, 6000);
-
-
-
+    },6000);
 }
+
